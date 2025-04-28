7568d51fa0ba9a3a8b48958019487e1c70a8b3f3
CY <908431711@qq.com>
2025-04-28 17:14:42 +0800

    4.28 进行初始化

---
+++ main_project/main.py
@@ -0,0 +1,6 @@
+import time
+import gc
+from config import PH_COLOR_THRESHOLDS, CONFIG
+from hardware.sensor_manager import SensorManager
+from hardware.uart_manager import UARTManager
+from hardware.button_handler import ButtonHandler
